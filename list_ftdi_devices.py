168a05f61970b61c6647e212018171ee5bcc8339
Pat Deegan <pat.devel@psychogenic.com>
2023-12-13 10:10:34 -0500

    Augmented to allow listing of compatible devices, and easier tweak in program.py

---
+++ list_ftdi_devices.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import os
+
+print("Searching for compatible devices...")
+os.system(f"python3 flasher/flash_util.py  --list") 
--- program.py
+++ program.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 
+
+DeviceURI = 'ftdi://ftdi:2232:TG110925/2'
 while True:
     input("Press enter to flash")
-    os.system(f"python3 flasher/flash_util.py  --write binaries/v2.2.3.bin") 
+    os.system(f"python3 flasher/flash_util.py  --uri {DeviceURI} --write binaries/v2.2.3.bin") 
